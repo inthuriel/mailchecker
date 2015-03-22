d8c509eb1b7ba9894fea92a04feda62cb764f994
Mikolaj Niedbala <kontakt@mikolajniedbala.pl>
2015-03-22 23:05:18 +0100

    Speedup library, remove unnecessary files

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
  
 setup(
     name = "mailchecker",
-    version = "1.12",
+    version = "1.15",
     description='Simple liblary for existence of email checking',
     author='Mikolaj [Metatron] Niedbala',
     author_email='metatron@metatron.com.pl',
     url='http://dev.metatron.com.pl',    
-    packages = find_packages()
+    packages= find_packages()
     )
